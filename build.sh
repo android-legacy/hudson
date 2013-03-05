e62faa641db5ea7845a3411e666413502632eef3
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-03-04 22:17:29 -0500

    Fix my logic

--- build.sh
+++ build.sh
@@ -84,7 +84,7 @@ fi
 git config --global user.name $(whoami)@$NODE_NAME
 git config --global user.email jenkins@androidarmv6.org
 
-if [ "$REPO_BRANCH" =~ "jellybean" ]; then 
+if [ "$REPO_BRANCH" == "jellybean" ]; then 
    JENKINS_BUILD_DIR=jellybean
 else
    JENKINS_BUILD_DIR=$REPO_BRANCH
@@ -114,11 +114,12 @@ repo init -u $SYNC_PROTO://github.com/androidarmv6/android.git -b $CORE_BRANCH $
 check_result "repo init failed."
 
 # make sure ccache is in PATH
-if [ "$REPO_BRANCH" =~ "jellybean" ]
+if [ "$REPO_BRANCH" == "jellybean" ]
 then
 export PATH="$PATH:/opt/local/bin/:$PWD/prebuilts/misc/$(uname|awk '{print tolower($0)}')-x86/ccache"
 export CCACHE_DIR=~/.jb_ccache
-elif [ "$REPO_BRANCH" =~ "cm-10.1" ]
+elif [ "$REPO_BRANCH" =~ "cm-10" ]
+then
 export PATH="$PATH:/opt/local/bin/:$PWD/prebuilts/misc/$(uname|awk '{print tolower($0)}')-x86/ccache"
 export CCACHE_DIR=~/.jb2_ccache
 else
