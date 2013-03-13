e2dbc1134bb0e2a78310a88f35890576f0c8934e
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-03-12 23:32:28 -0300

    Let's speed this up, shall we?

--- build.sh
+++ build.sh
@@ -264,7 +264,7 @@ fi
 
 echo "$REPO_BRANCH-$CORE_BRANCH$RELEASE_MANIFEST" > .last_branch
 
-time mka bacon recoveryzip recoveryimage #checkapi
+time mka -j16 bacon recoveryzip recoveryimage #checkapi
 check_result "Build failed."
 
 for f in $(ls $OUT/cm-*.zip*)
