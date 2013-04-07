ceb8e34891ba34e2d5b46e38cf282f2a21267503
AndroidMeda <hephappy@gmail.com>
2013-04-07 13:23:52 +0300

    Don't add CHANGES.txt
    - We don't use CMUpdater
    - CHANGES.txt is very huge for some devices (7MB)

--- image-auto-bits.mk
+++ image-auto-bits.mk
@@ -1,3 +1,5 @@
 PRODUCT_COPY_FILES += \
-    $(WORKSPACE)/archive/manifest.xml:system/etc/build-manifest.xml \
-    $(WORKSPACE)/archive/CHANGES.txt:system/etc/CHANGES.txt
+    $(WORKSPACE)/archive/manifest.xml:system/etc/build-manifest.xml
+
+#PRODUCT_COPY_FILES += \
+#    $(WORKSPACE)/archive/CHANGES.txt:system/etc/CHANGES.txt
