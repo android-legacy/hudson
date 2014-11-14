afac215c3617d3cc371647ad961297afaf559edd
erikcas <erikcas1972@gmail.com>
2014-11-14 07:35:24 +0100

    make

--- build.sh
+++ build.sh
@@ -370,8 +370,6 @@ git cherry-pick 5898f2e789800fb196ce94532eef033e7d7e60b3
 cd ../../../
 
 make -j16
-make -j16 otatools
-make -j16 otapackage
 # recoveryzip recoveryimage checkapi
 check_result "Build failed."
 
