837d9908ab7ef78ea80ebf04bab119d97a0ea7fe
erikcas <erikcas1972@gmail.com>
2014-12-02 14:45:51 +0100

    adjust ccache and save some diskspace :P

--- build.sh
+++ build.sh
@@ -197,7 +197,7 @@ if [ $USE_CCACHE -eq 1 ]
 then
   # make sure ccache is in PATH
   export PATH="$PATH:/opt/local/bin/:$PWD/prebuilts/misc/$(uname|awk '{print tolower($0)}')-x86/ccache"
-  export CCACHE_DIR=~/ccache-jenkins/$JOB_NAME/$REPO_BRANCH
+  export CCACHE_DIR=/ccache-erikcas
   mkdir -p $CCACHE_DIR
 fi
 
@@ -301,7 +301,7 @@ if [ $USE_CCACHE -eq 1 ]
 then
   if [ ! "$(ccache -s|grep -E 'max cache size'|awk '{print $4}')" = "64.0" ]
   then
-    ccache -M 64G
+    ccache -M 8G
   fi
   echo "============================================"
   ccache -s
