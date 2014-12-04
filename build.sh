e358708a1f80a25a04fcea0c2f2e54c338a90521
erikcas <erikcas1972@gmail.com>
2014-12-04 07:06:10 +0100

    esthetical

--- build.sh
+++ build.sh
@@ -104,13 +104,15 @@ then
 fi
 
 # colorization fix in Jenkins
+export CL_RST="\"\033[0m\""
+export CL_BLK="\"\033[30m\""
 export CL_RED="\"\033[31m\""
 export CL_GRN="\"\033[32m\""
 export CL_YLW="\"\033[33m\""
 export CL_BLU="\"\033[34m\""
 export CL_MAG="\"\033[35m\""
 export CL_CYN="\"\033[36m\""
-export CL_RST="\"\033[0m\""
+export CL_WHT="\"\033[37m\""
 
 cd $WORKSPACE
 rm -rf archive
